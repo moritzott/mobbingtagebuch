63e8c48043e632be8e3935275f60c3dc9a24d87e
Moritz Ott <moritz-benjamin.ott@web.de>
2023-10-02 18:02:17 +0200

    feat(app): add basic template with header, footer and first page

---
+++ src/app/home/component/new-project-card/new-project-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-project-card',
+  templateUrl: './new-project-card.component.html',
+  styleUrls: ['./new-project-card.component.css']
+})
+export class NewProjectCardComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageContainerComponent } from './components/page-container/page-container.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageContainerComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    PageContainerComponent,
+    FooterComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
