d672f9b1a5a25cc1de25679521105617469477a5
Moritz Ott <moritz-benjamin.ott@web.de>
2023-11-05 14:12:27 +0100

    feat(export): enable export of project via file

--- src/app/export/pages/file/file.component.ts
+++ src/app/export/pages/file/file.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Project } from 'src/app/shared/interfaces/project';
+import { ProjectService } from 'src/app/shared/services/project.service';
 
 @Component({
   selector: 'app-file',
   templateUrl: './file.component.html',
   styleUrls: ['./file.component.css']
 })
-export class FileComponent {
+export class FileComponent implements OnInit, OnDestroy {
+  @ViewChild('downloadLink') downloadLink! : ElementRef<HTMLAnchorElement>;
+
+  public projects: Project[] = [];
+  private projectSubscription!: Subscription;
+
+  constructor(private projectService: ProjectService) { }
+
+  /**
+   * Get all projects and save them in a variable and as downloadable file.
+   */
+  ngOnInit(): void {
+    this.projectSubscription = this.projectService.getProjects().subscribe(projects => this.projects = projects);
+    console.log('Projekte', this.projects);
+
+    setTimeout(() => {
+      const blob = new Blob([JSON.stringify(this.projects)], { type: "application/json" });
+      this.downloadLink.nativeElement.download = 'projects.json';
+      this.downloadLink.nativeElement.href = window.URL.createObjectURL(blob);
+      this.downloadLink.nativeElement.dataset['downloadurl'] = ["application/json", this.downloadLink.nativeElement.download, this.downloadLink.nativeElement.href].join(":");
+    });
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.projectSubscription.unsubscribe();
+  }
+
+
 
 }
