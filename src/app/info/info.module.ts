1c7ef4d76a9d1f0de3c49bb90149f5621cd4b8d4
Moritz Ott <moritz-benjamin.ott@web.de>
2023-11-12 18:25:48 +0100

    feat(app): :lipstick: add links to footer and change naming of entries

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InfoRoutingModule } from './info-routing.module';
 import { InfoComponent } from './info.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LicensesComponent } from './pages/licenses/licenses.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 
 @NgModule({
   declarations: [
-    InfoComponent
+    InfoComponent,
+    AboutComponent,
+    LicensesComponent,
+    ImprintComponent,
+    PrivacyComponent
   ],
   imports: [
     CommonModule,
